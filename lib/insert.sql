3d2ed7600fe10c20d7ca0ea086a724c91e1dab2d
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-11-05 16:26:15 +0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,33 @@
+-- lib/insert.sql
+
+-- Insert Mr. Chocolate (assuming that the table is named 'bears')
+INSERT INTO bears (name, sex)
+VALUES ('Mr. Chocolate', 'M');
+
+-- Insert Rowdy
+INSERT INTO bears (name, sex)
+VALUES ('Rowdy', 'M');
+
+-- Insert Tabitha
+INSERT INTO bears (name, sex)
+VALUES ('Tabitha', 'F');
+
+-- Insert Sergeant Brown
+INSERT INTO bears (name, sex)
+VALUES ('Sergeant Brown', 'M');
+
+-- Insert Melissa
+INSERT INTO bears (name, sex)
+VALUES ('Melissa', 'F');
+
+-- Insert Grinch
+INSERT INTO bears (name, sex)
+VALUES ('Grinch', 'M');
+
+-- Insert Wendy
+INSERT INTO bears (name, sex)
+VALUES ('Wendy', 'F');
+
+-- Insert unnamed (assuming NULL value for name)
+INSERT INTO bears (sex)
+VALUES ('M');
